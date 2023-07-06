bc9f06d9b3c7eaae150677bfb5fa91168722d3a0
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-07-06 13:50:28 +0300

    TekCiftKonrol

---
+++ TekCiftKontrol/TekCiftKontrol/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TekCiftKontrol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Kullanıcıdan alınan bir sayının tek çift kontrolünü yapınız.
+            Console.Write("Bir sayı giriniz :");
+            int sayi = int.Parse(Console.ReadLine());
+
+            if ((sayi % 2) == 0)
+            {
+                Console.WriteLine("Girilen sayı çift sayıdır.");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayı tek sayıdır.");
+            }
+        }
+    }
+}
